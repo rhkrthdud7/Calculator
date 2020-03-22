dbf9681f2a6b572446077550f274abfb4a080df4
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-03-22 13:13:10 +0900

    init

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Soso on 22/03/2020.
+//  Copyright © 2020 Soso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
